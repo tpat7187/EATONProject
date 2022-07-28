686f28c1d129798b1a83000ab3ee992b79413c9d
tpat7187 <corranpaterson@live.com>
2022-07-28 02:33:31 +0100

    cleaning some data

--- model.py
+++ model.py
@@ -9,13 +9,19 @@ import os
 
 path = "dataset/AdvertisingData.xls"
 df = pd.read_excel(path)
+df = df.drop(columns = ['Downloads', 'Watchtime', 'Destination URL', 'Hosting URL'])
 df.to_csv("dataset/Advertising.csv", index=False)
-test = pd.read_csv("dataset/Advertising.csv")
+test = pd.read_csv("dataset/Advertising.csv", low_memory = False, usecols = lambda c: not c.startswith('Unnamed:'))
 
-print(test)
 
-sns.relplot(x="Impressions", y = "Clicks", data = test)
-plt.show()
+print(test.axes)
+
+
+#if you want to see the test of the plot
+#sns.relplot(x="Impressions", y = "Clicks", data = test)
+#plt.show()
+
+
 
 
 
