b74af3dda4307a910b3350036125e1791a1ce89b
tpat7187 <corranpaterson@live.com>
2022-08-03 13:32:20 +0100

    notebook + date columns

--- model.py
+++ model.py
@@ -8,63 +8,50 @@ import seaborn as sns
 import os 
 
 
-#dataparser
-dateparse = lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
-
 
 df = pd.read_csv("dataset/Advertising.csv", low_memory = False, usecols = lambda c: not c.startswith('Unnamed:'), parse_dates=['Week'])
 
 
-#making a dataframe of only google ad data
-row_idx = []
+# Restarting rn because im sad 0
+#print(df.AdType.unique())
+test_frame = df.loc[df.AdType == 'Email'].reset_index(drop = True)
+
+# splitting dataset by year
+
+cringe = []
+date15, date16, date17, date18 = 0, 0, 0, 0
+
 for x in range(len(df)):
-  if df.loc[x, 'AdType'] == 'Google Ad Words':
-    row_idx.append(x)
+  year = df.loc[x, 'Week'].year
+  cringe.append(year)
+  if year == 2015:
+    date15 += 1
+    print(df.loc[x])
+  if year == 2016:
+    date16 += 1
+  if year == 2017:
+    date17 += 1
+  if year == 2018:
+    date18 += 1
 
-google_df = df.loc[row_idx]
-#print(google_df)
-google_df['year'] =  pd.DatetimeIndex(google_df['Week']).year
 
-#looking at dates year-month-day
-#df['year'] = pd.DatetimeIndex(df['Week']).year
-#print(df)
+print(date15, date16, date17, date18)
+#print(set(cringe))
 
+# Could do it by month instead of week it will probably be easier
+#print(test_frame.Week.nunique(), len(test_frame))
 
-# TODO: look at engaements per year by week ? -> by ad type
-list_idx = []
-list_idx2 = []
-null_list = []
+months = [0]*12
 
-for x in range(len(df)): 
-  if df.loc[x, 'Week'].year == 2016:
-    list_idx.append(x)
-  if df.loc[x, 'Week'].year == 2017:
-    list_idx2.append(x)
-  if df.loc[x, 'Week'].year == 2018: 
-    null_list.append(x)
+for x in range(len(test_frame)):
+  month = test_frame.loc[x, 'Week'].month
+  #print(month)
+  months[month-1] += test_frame.loc[x, 'Impressions']
 
-print(null_list)
-
-print(len(list_idx), len(list_idx2))
-first_year = df.loc[list_idx]
-second_year = df.loc[list_idx2]
-third_year = df.loc[null_list]
-
-#all ad types in 2017 are used in 2016 
-print(second_year.columns == first_year.columns)
-print(third_year.AdType.unique())
-print(len(second_year) + len(first_year), len(df))
-
-# if you want to look at the graph
+print(months)
 '''
-print(segments, df.Segment.nunique())
-print('Publications', publications)
+plt.plot(months)
+plt.show()
 '''
-
-
-
-
-
-
 
 
